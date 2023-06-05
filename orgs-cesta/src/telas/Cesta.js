e88aa6ad18a21c2b2863d863864ec4fdb5f46cba
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2023-06-04 23:23:53 -0300

    att

--- orgs-cesta/src/telas/Cesta.js
+++ orgs-cesta/src/telas/Cesta.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Image, StyleSheet, Dimensions, Text } from 'react-native';
+import { Image, StyleSheet, Dimensions, Text, View } from 'react-native';
+
+//IMAGENS
 import topo from '../../assets/topo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -8,6 +11,16 @@ export default function cesta(){
     return <>
         <Image source={topo} style={estilos.topo}/>
         <Text style={estilos.titulo}>Detalhe da cesta</Text>
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de verduras</Text>
+            <View style={estilos.fazenda}>
+            <Image source={logo} style={estilos.imagemFazenda}/>
+            <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+            <Text style={estilos.descricao}>Uma cesta com produtos slecionados cuidadosamente da fazendo direto para sua cozinha</Text>
+            <Text style={estilos.preco}>R$40,00</Text>
+
+        </View>
     </>
 }
 
@@ -24,7 +37,49 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26,
         color:"white",
-        fontWeight: "bold",
         padding: 16,
+        fontFamily: "MontserratBold",
+
     },
+    cesta:{
+        paddingVertical:8,
+        paddingHorizontal:16,
+    },
+    nome:{
+        fontSize: 26,
+        lineHeight: 42,
+        color: "#464646",
+        fontFamily:"MontserratBold",
+    },
+    fazenda:{
+        flexDirection:"row",
+        paddingHorizontal: 12,
+    },
+    nomeFazenda:{
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: "MontserratRegular",
+
+    },
+    imagemFazenda:{
+        width: 32,
+        height: 32,
+    },
+    descricao:{
+        color:"#A3A3A3",
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: "MontserratRegular",
+
+    },
+    preco:{
+        fontFamily: "MontserratBold",
+        color: "#2A9F85",
+        fontSize: 26,
+        marginTop:8,
+        lineHeight: 42,
+    },
+
+    
   });
