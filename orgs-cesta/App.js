9b3f7a4e6e6843d41edb9d1a4b73d132924ed6ae
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2023-06-09 17:29:50 -0300

    AppLoading added

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -2,6 +2,7 @@ import { StatusBar, Text, SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
 import mock from './src/mocks/cesta';
 
+import AppLoading from 'expo-app-loading';
 import {useFonts, Montserrat_400Regular, Montserrat_700Bold} from '@expo-google-fonts/montserrat';
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
   });
 
   if(!fontCarregada){
-    return<View/>
+    return<AppLoading/>
   }
   return (
     <SafeAreaView>
